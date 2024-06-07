15977cd5ec9e3d29be874e6a8b4c740d1ac54ccc
ghapviniciussouza <vinicius.souza@ghap.com.br>
2024-06-07 19:10:10 -0300

    feat: created users page and crud to admin user, also independent edit user data on sidebar, for users

---
+++ src/components/pages/sidebar/editUser.tsx
@@ -0,0 +1,173 @@
+import React from 'react';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { useForm } from 'react-hook-form';
+import {
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogPortal,
+} from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { api } from '@/services/api';
+import { useMutation, useQueryClient } from 'react-query';
+import { toast } from '@/components/ui/use-toast';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { PasswordInput } from '@/components/ui/password-input';
+
+interface EditUserProps {
+  onClose: () => void;
+  userData: any;
+}
+
+interface FormInputs {
+  email: string;
+  name: string;
+  username: string;
+  password: string;
+  confirmPassword: string;
+  targetUserId: string;
+}
+
+const EditUserSidebar: React.FC<EditUserProps> = ({ onClose, userData }) => {
+  const queryClient = useQueryClient();
+
+  const FormSchema = z
+    .object({
+      email: z.string().email({ message: 'Email inválido' }),
+      name: z.string(),
+      username: z.string(),
+      password: z.string().optional().nullable(),
+      confirmPassword: z.string().optional().nullable(),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+      message: 'As senhas não correspondem',
+      path: ['confirmPassword'],
+    });
+
+  const form = useForm<FormInputs>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      email: userData.email,
+      name: userData.name,
+      username: userData.username,
+      targetUserId: userData.id,
+    },
+  });
+
+  const { mutate, isLoading } = useMutation(
+    async (data: FormInputs) => {
+      await api.put(`/users/`, data);
+    },
+    {
+      onError: (error: any) => {
+        toast({
+          title: 'Erro ao atualizar seus dados',
+          description: `${error.message}`,
+        });
+      },
+      onSuccess: async () => {
+        await queryClient.refetchQueries('@userData');
+
+        toast({
+          title: 'Sucesso!',
+          description: 'Seus dados foram atualizados!',
+        });
+
+        onClose();
+      },
+    },
+  );
+
+  return (
+    <div className="mt-32 flex h-screen justify-center">
+      <DialogPortal>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Atualize seus dados</DialogTitle>
+            <DialogDescription>Altere seu dados pessoais.</DialogDescription>
+          </DialogHeader>
+          <Form {...form}>
+            <form className="grid gap-4 py-4" onSubmit={form.handleSubmit((data) => mutate(data))}>
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Informe o Email" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Nome</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Informe o nome" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Usuário</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Informe o usuário" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Senha</FormLabel>
+                    <FormControl>
+                      <PasswordInput placeholder="Informe a nova senha" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="confirmPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Confirmar senha</FormLabel>
+                    <FormControl>
+                      <PasswordInput placeholder="Confirme sua nova senha" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <DialogFooter>
+                <Button type="submit" disabled={isLoading}>
+                  Atualizar
+                </Button>
+              </DialogFooter>
+            </form>
+          </Form>
+        </DialogContent>
+      </DialogPortal>
+    </div>
+  );
+};
+
+export default EditUserSidebar;
---
+++ src/pages/usuarios/index.tsx
@@ -0,0 +1,84 @@
+import { Button } from '@/components/ui/button';
+import { Label } from '@/components/ui/label';
+import * as Dialog from '@radix-ui/react-dialog';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import { Pencil } from 'phosphor-react';
+
+import { useState } from 'react';
+
+import { api } from '@/services/api';
+import { useQuery } from 'react-query';
+import RegisterUser from '@/components/pages/usuarios/registerOrEdit';
+
+export default function Users() {
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [userSelected, setUserSelected] = useState<any | null>(null);
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+
+  async function fetchUsers() {
+    const response = await api.get('/users/all');
+    return response.data;
+  }
+
+  const { data: users } = useQuery('@users', fetchUsers, {
+    refetchOnWindowFocus: false,
+  });
+
+  const handleEdituser = (company: any) => {
+    setUserSelected(company);
+    setIsEditing(true);
+    setShowModal(true);
+  };
+
+  const handleAddUser = () => {
+    setUserSelected(null);
+    setIsEditing(false);
+    setShowModal(true);
+  };
+
+  return (
+    <div className="ml-0 flex h-screen">
+      <div className="w-full max-w-full overflow-auto p-4 sm:ml-[270px]">
+        <h2 className="mt-6 border-b pb-2 text-3xl font-semibold tracking-tight">Usuários Cadastrados</h2>
+
+        <div className="my-8 flex justify-end">
+          <Button variant="outline" onClick={handleAddUser}>
+            Adicionar Usuário
+          </Button>
+        </div>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Nome</TableHead>
+              <TableHead>Email</TableHead>
+              <TableHead>Tipo</TableHead>
+              <TableHead>Ativo</TableHead>
+              <TableHead>Editar</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {users?.map((user) => (
+              <TableRow key={user.id}>
+                <TableCell className="font-medium">{user.name}</TableCell>
+                <TableCell>{user.email}</TableCell>
+                <TableCell>{user.type}</TableCell>
+                <TableCell>{user.isActive ? 'Sim' : 'Não'}</TableCell>
+                <TableCell>
+                  <Button onClick={() => handleEdituser(user)}>
+                    <Pencil className="mr-2" />
+                    Editar
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+      {showModal && (
+        <Dialog.Root open={showModal} onOpenChange={setShowModal}>
+          <RegisterUser onClose={() => setShowModal(false)} isEditing={isEditing} userSelected={userSelected} />
+        </Dialog.Root>
+      )}
+    </div>
+  );
+}
