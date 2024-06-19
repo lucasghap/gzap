cfe2a81e105ebd4f59ce18c1843cf9f92e21181e
ghapviniciussouza <vinicius.souza@ghap.com.br>
2024-06-19 15:06:59 -0300

    fix: adjusted darkmode exception, on login page, also redirect when not logged

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,35 +1,39 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import { AppProps } from 'next/app';
 import { QueryClientProvider } from 'react-query';
 import { queryClient } from '@/services/queryClient';
 import '../styles/global.css';
 import { Sidebar } from '@/components/sidebar';
-import { useRouter } from 'next/router';
 import { Toaster } from '@/components/ui/toaster';
 import { ThemeProvider } from '@/components/theme-provider';
 
 const Home: React.FC<AppProps> = ({ Component, pageProps }) => {
-  const { pathname } = useRouter();
+  const { pathname, push } = useRouter();
 
   const SCREENS_WITHOUT_MENU = ['/'];
 
   const shouldRenderSidebar = !SCREENS_WITHOUT_MENU.includes(pathname);
-  const applyTheme = pathname !== '/';
+
+  useEffect(() => {
+    const token = sessionStorage.getItem('tkn_gzap');
+    if (token && pathname === '/') {
+      push('/gzap');
+    }
+  }, [pathname, push]);
 
   return (
     <QueryClientProvider client={queryClient}>
-      {applyTheme ? (
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
-          {shouldRenderSidebar && <Sidebar />}
-          <Component {...pageProps} />
-          <Toaster />
-        </ThemeProvider>
-      ) : (
-        <>
-          {shouldRenderSidebar && <Sidebar />}
-          <Component {...pageProps} />
-          <Toaster />
-        </>
-      )}
+      <ThemeProvider
+        attribute="class"
+        forcedTheme={pathname === '/' ? 'light' : undefined}
+        defaultTheme="light"
+        enableSystem
+      >
+        {shouldRenderSidebar && <Sidebar />}
+        <Component {...pageProps} />
+        <Toaster />
+      </ThemeProvider>
     </QueryClientProvider>
   );
 };
